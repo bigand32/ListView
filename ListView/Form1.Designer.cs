9f3d2017bc2ffdab7ac39a5299aa088683b40748
Minji Bake <bigand32@gmail.com>
2019-05-27 14:01:35 +0900

    #2 리스트뷰 항목  추가 보기 구현

--- ListView/Form1.Designer.cs
+++ ListView/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.lvView.UseCompatibleStateImageBehavior = false;
             this.lvView.View = System.Windows.Forms.View.Details;
             this.lvView.SelectedIndexChanged += new System.EventHandler(this.lvView_SelectedIndexChanged);
+            this.lvView.Click += new System.EventHandler(this.lvView_Click);
             // 
             // lblName
             // 
@@ -113,6 +114,7 @@
             this.btnAdd.TabIndex = 7;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // chName
             // 
--- ListView/Form1.cs
+++ ListView/Form1.cs
@@ -11,6 +11,61 @@ namespace ListView
 {
     public partial class Form1 : Form
     {
+        String strName, strAge, strWork;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(TextCheck())
+            {
+                strName = this.txtName.Text;
+                strAge = this.txtAge.Text;
+                strWork = this.txtWork.Text;
+                ListViewItem lvi =
+                    new ListViewItem(
+                        new String[] { strName, strAge, strWork });
+                this.lvView.Items.Add(lvi);
+                this.txtName.Text = "";
+                this.txtAge.Text = "";
+                this.txtWork.Text = "";
+                this.txtName.Focus();
+            }
+        }
+        private bool TextCheck()
+        {
+            if (this.txtName.Text == "")
+            {
+                MessageBox.Show("이름을 입력해 주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtName.Focus();
+                return false;
+            }
+            if (this.txtAge.Text == "")
+            {
+                MessageBox.Show("나이를 입력해 주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtAge.Focus();
+                return false;
+            }
+            if (this.txtWork.Text == "")
+            {
+                MessageBox.Show("직업을 입력해 주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtWork.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void lvView_Click(object sender, EventArgs e)
+        {
+            if (this.lvView.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("항목을 선택하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                ListViewItem lvi = this.lvView.SelectedItems[0];
+                MessageBox.Show("이름:" + lvi.SubItems[0].Text + "\n나이:" + lvi.SubItems[1].Text + "\n직업:" + lvi.SubItems[2].Text, "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
